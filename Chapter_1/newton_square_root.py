dece0e53cb63e0e99710502cc8db5398997fb8c2
Gabriel Barberini <gabriel.barberini@sumup.com>
2020-02-05 16:43:10 -0300

    newton square root problem generalized

--- Chapter_1/newton_square_root.py
+++ Chapter_1/newton_square_root.py
@@ -1,5 +1,7 @@
 #Newton method for finding square roots. y = ( x + a/x )/2 where 'x' is an initial approximate guess for the square root and 'a' is the number which we want to find the square from.
 
+import math
+
 def newton_square(a,x):    
     """
 'a' is the number you want to determine the square root
@@ -12,4 +14,30 @@ def newton_square(a,x):
             break
         x = y
 
-newton_square(180, 13) 
+def mysqrt(a):
+    """
+this function is an encapsulation of the newton_square algorithm in which the approximate guess is taken recursively without any further input""" 
+    
+    if a <= 1:
+        return 1
+
+    else:   
+        x = mysqrt(a-1)
+        while True:
+            y = (x + a/x)/2
+            if x == y:
+                return x
+            x = y
+
+def test_square_root():
+    print(mysqrt(1),math.sqrt(1),abs(mysqrt(1)-math.sqrt(1)))
+    print(mysqrt(2),math.sqrt(2),abs(mysqrt(2)-math.sqrt(2)))
+    print(mysqrt(3),math.sqrt(3),abs(mysqrt(3)-math.sqrt(3)))
+    print(mysqrt(4),math.sqrt(4),abs(mysqrt(4)-math.sqrt(4)))
+    print(mysqrt(5),math.sqrt(5),abs(mysqrt(5)-math.sqrt(5)))
+    print(mysqrt(6),math.sqrt(6),abs(mysqrt(6)-math.sqrt(6)))
+    print(mysqrt(7),math.sqrt(7),abs(mysqrt(7)-math.sqrt(7)))
+    print(mysqrt(8),math.sqrt(8),abs(mysqrt(8)-math.sqrt(8)))
+    print(mysqrt(9),math.sqrt(9),abs(mysqrt(9)-math.sqrt(9)))
+
+test_square_root()
