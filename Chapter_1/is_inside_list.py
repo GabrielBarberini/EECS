0ad5027c2495e2c6ccf9d7101639d7a386b97a28
Gabriel Barberini <gabriel.barberini@sumup.com>
2020-06-28 21:07:34 -0300

    binary search inside list + ex11_5

---
+++ Chapter_1/is_inside_list.py
@@ -0,0 +1,9 @@
+#caught from thinkpython2
+
+import bisect
+
+def is_inside_list(word_list, word):
+    i = bisect.bisect_left(word_list, word):
+        if len(word_list) == i:
+            return False
+        return word_list[i] == word
