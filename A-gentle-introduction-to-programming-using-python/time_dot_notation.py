3c9b39424149214950d0b523bbf87d7d6b623667
Gabriel Barberini <gabriel.barberini@sumup.com>
2020-08-14 21:22:12 -0300

    change time_dot_notation exercise description

--- A-gentle-introduction-to-programming-using-python/time_dot_notation.py
+++ A-gentle-introduction-to-programming-using-python/time_dot_notation.py
@@ -1,5 +1,4 @@
-"""As an exercise, write a function called print_time that takes a Time object and prints it in the form hour:minute:second. Hint: the format sequence '%.2d' prints an integer using at least two digits, including a leading zero if necessary.
-Write a boolean function called is_after that takes two Time objects, t1 and t2, and re- turns True if t1 follows t2 chronologically and False otherwise. Challenge: don’t use an if statement."""
+"""As an exercise, write a function called print_time that takes a Time object and prints it in the form hour:minute:second. Hint: the format sequence '%.2d' prints an integer using at least two digits, including a leading zero if necessary."""
 
 class Time:
     hour = 3
