71d3e97f91ee841bbe9279a9a549b0d0f6595085
GabrielBarberini <gabriel.barberini@sumup.com>
2022-07-04 15:55:33 -0300

    renames graph test

--- 6-00sc/FingerExercises/graph/sample_graph_test.py
+++ 6-00sc/FingerExercises/graph/sample_graph_test.py
@@ -1,5 +1,6 @@
 import graph
 from depth_first_search import shortest_path
+from depth_first_search import print_path 
 
 sample_graph = graph.Graph()
 long_node_path = [graph.Node("A"), graph.Node("B"), graph.Node("C"), graph.Node("D"), graph.Node("E")]
@@ -7,7 +8,8 @@ short_node_path = [graph.Node("BL"), graph.Node("CL")]
 edges = [ graph.Edge(long_node_path[0], long_node_path[1]), graph.Edge(long_node_path[1], long_node_path[2]), 
           graph.Edge(long_node_path[2], long_node_path[3]), graph.Edge(long_node_path[3], long_node_path[4]), 
           graph.Edge(long_node_path[0], short_node_path[0]), 
-          graph.Edge(short_node_path[0], short_node_path[1])
+          graph.Edge(short_node_path[0], short_node_path[1]),
+          graph.Edge(short_node_path[1], long_node_path[4])
         ]
 
 for node in long_node_path:
@@ -18,5 +20,5 @@ for node in short_node_path:
 for edge in edges:
     sample_graph.add_edge(edge)
 
-shortest_path(sample_graph, long_node_path[0], long_node_path[4], True)
+print("Shortest path is: " + print_path(shortest_path(sample_graph, long_node_path[0], long_node_path[4], True)))
 
---
+++ readings/Cryptography/Articles/RSA.md
@@ -0,0 +1,9 @@
+---
+Category:: #article
+Tags:: #course #infra #devops #docker
+Links:: [[RSA]], [[Asymmetric Cryptography]]
+---
+
+# Rivest–Shamir–Adleman
+## Complementay
+- [Difference between RSA cryptography and RSA Signature](https://www.cs.cornell.edu/courses/cs5430/2015sp/notes/rsa_sign_vs_dec.php)
