f2f6cf5331fffac62b05a05ff34b4aa1622cb393
GabrielBarberini <gabriel.barberini@sumup.com>
2022-07-08 20:11:31 -0300

    updates memoization solution

--- 6-00sc/FingerExercises/dynamic/tabular.py
+++ 6-00sc/FingerExercises/dynamic/tabular.py
@@ -1,8 +1,7 @@
-def wo_make_change(coin_vals, change):
+def make_change(coin_vals, change):
     """coin_vals is a list of positive ints and coin_vals[0] = 1, change is a positive int.
        Return the minimum number of coins needed to have a set of coins the values of which sum to change. 
-       Coins may be used more than once. For example, make_change([1, 5, 8], 11) should return 3.
-    """
+       Coins may be used more than once. For example, make_change([1, 5, 8], 11) should return 3. """
 
     #solve trivialities
     if change == 0 or change == 1 or len(coin_vals) == 1:
@@ -21,16 +20,4 @@ def wo_make_change(coin_vals, change):
         return change/coin_vals[0]
 
     #begin
-    step = 1 
-    possibilities = set() 
-
-    while step <= change:
-        for i in range(change):
-            if (change - i)/step in coin_vals:
-                possibilities.add(wo_make_change(coin_vals, i) + step) #remainder changes + (change-i)
-        step += 1
-
-    result = min(possibilities)
-    return result
-
-    # Theta( f(a,b) ) = b^2 * a * log(a)
+    #WIP
--- 6-00sc/FingerExercises/dynamic/with_memoization.py
+++ 6-00sc/FingerExercises/dynamic/with_memoization.py
@@ -2,12 +2,27 @@ def make_change(coin_vals, change):
     """coin_vals is a list of positive ints and coin_vals[0] = 1, change is a positive int.
        Return the minimum number of coins needed to have a set of coins the values of which sum to change. 
        Coins may be used more than once. For example, make_change([1, 5, 8], 11) should return 3.
-                                                                                                 """
-    if change == 0 or change == 1:
-        return change #change in coin_vals
 
+    """
+    #solve trivialities
+    if change == 0 or change == 1 or len(coin_vals) == 1:
+        return change #change in coin_vals or only 1 in coin_vals
+
+    coin_vals.sort(reverse=True)
+
+    for number in coin_vals:
+        if number > change:
+            coin_vals.remove(number)
+
+    if change in coin_vals:
+        return 1
+
+    if change % coin_vals[0] == 0:
+        return change/coin_vals[0]
+
+    #begin
     step = 1 
-    possibilities = []
+    possibilities = set() 
 
     try:
         return cached_changes(tuple(coin_vals), change)
@@ -16,11 +31,12 @@ def make_change(coin_vals, change):
         while step <= change:
             for i in range(change):
                 if (change - i)/step in coin_vals:
-                    possibilities.append(make_change(coin_vals, i) + step) #remainder changes + (change-i)
+                    possibilities.add(make_change(coin_vals, i) + step) #remainder changes + (change-i)
             step += 1
 
         result = min(possibilities)
         cached_changes(tuple(coin_vals), change, result)
+
         return result
 
         # Theta( f(a,b) ) = b^2 * a * log(a)
