604136a85fc24e4ac5521d70115d1315939c5518
GabrielBarberini <gabriel.barberini@sumup.com>
2022-07-14 15:05:35 -0300

    updates tabular solutions

--- 6-00sc/FingerExercises/dynamic/test.py
+++ 6-00sc/FingerExercises/dynamic/test.py
@@ -1,22 +1,22 @@
 from datetime import datetime
-from tabular import tabu_make_change
-from memoization import memo_make_change
+from tabular_all import tabuall_make_change
+from tabular_ish import tabish_make_change
 
-print("Memoization")
+print("Tabular-ish")
 before = datetime.now()
-print("coin_val = range(1,50), change = 250: ", memo_make_change(list(range(1,50)), 250))
-print("coin_val = [1, 3, 4, 5], change = 7: ", memo_make_change([1, 3, 4, 5], 7))
-print("coin_val = [1], change = 250: ", memo_make_change([1], 250))
+print("coin_val = range(1,50), change = 250: ", tabish_make_change(list(range(1,50)), 250))
+print("coin_val = [1, 3, 4, 5], change = 7: ", tabish_make_change([1, 3, 4, 5], 7))
+print("coin_val = [1], change = 250: ", tabish_make_change([1], 250))
 after = datetime.now()
 print("Before call:", before)
 print("After call:", after)
 print("Diff:", after-before, "\n")
 
-print("Tabular")
+print("Tabular-all")
 before = datetime.now()
-print("coin_val = range(1,50), change = 250: ", tabu_make_change(list(range(1,50)), 250))
-print("coin_val = [1, 3, 4, 5], change = 7: ", tabu_make_change([1, 3, 4, 5], 7))
-print("coin_val = [1], change = 250: ", tabu_make_change([1], 250))
+print("coin_val = range(1,50), change = 250: ", tabuall_make_change(list(range(1,50)), 250))
+print("coin_val = [1, 3, 4, 5], change = 7: ", tabuall_make_change([1, 3, 4, 5], 7))
+print("coin_val = [1], change = 250: ", tabuall_make_change([1], 250))
 after = datetime.now()
 print("Before call:", before)
 print("After call:", after)
