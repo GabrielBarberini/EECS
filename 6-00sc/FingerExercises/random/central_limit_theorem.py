330c6ae56706db90224237baa251cd74451ddbf8
GabrielBarberini <gabriel.barberini@sumup.com>
2022-08-04 21:10:35 -0300

    clarify application of TLC, SEM and SE with confidence interval

--- 6-00sc/FingerExercises/random/central_limit_theorem.py
+++ 6-00sc/FingerExercises/random/central_limit_theorem.py
@@ -55,7 +55,7 @@ x_means_mean = sum(x_means)/len(x_means)
 
 print("global mean: ", x_mean)
 print("means mean: ", x_means_mean)
-print("SE of pop:", np.std(x_pop))
+print("SD of pop:", np.std(x_pop))
 
 #Experiment 2:
 print("\n100 samples of random 500 elements (larger sample size)")
@@ -78,20 +78,20 @@ axes[0, 0].set_title("Large samples - 500 elements per sample")
 
 print("global mean x_2: ", x_2_mean)
 print("means mean x_2: ", x_2_means_mean)
-print("SE of pop:", np.std(x_2_pop))
+print("SD of pop:", np.std(x_2_pop))
 
 print()
 sample_x_2 = random.choice(x_2)
 sem_2 = np.std(sample_x_2)/(500**0.5)
 sample_2_mean = sum(sample_x_2)/len(sample_x_2)
-print("SEM: ", sem_2, "Mean of means SE:", np.std(x_2_means))
+print("SEM: ", sem_2, "Mean of means SD:", np.std(x_2_means))
 #95% probability
 print("Result by SE: Mean = ", x_2_means_mean, " +/- ", 1.96*np.std(x_2_means))
-print("Result by SEM: Mean = ", sample_2_mean, " +/- ", 1.96*sem_2)
+print("Result by SEM: Mean = ", sample_2_mean, "(sample mean) +/- ", 1.96*sem_2)
 print()
-print("Results diff: \nSEM Expected <= ", sem_2*1.96, "\nSEM = ", abs(x_2_mean-sample_2_mean), "\nSE Expected <= ", 1.96*np.std(x_2_means), "\nSE = ", abs(x_2_mean-x_2_means_mean))
+print("Results diff: \nExpected result by SEM <= ", sem_2*1.96, "\nResult by SEM = ", abs(x_2_mean-sample_2_mean), "\nExpected result by SE <= ", 1.96*np.std(x_2_means), "\nResult by SE = ", abs(x_2_mean-x_2_means_mean))
 
-#Obs: SEM is calculated over the elements of a single sample and this number approaches the SE of the mean of means if the sample is large enough.
+#Obs: SEM is calculated over the elements of a single sample and this number approaches the SD of the mean of means if the sample is large enough because the SD == SEM when the population is normally distributed (in this case our population is the means by which by TLC is expected to be normally distributed)
 
 #Experiment 3:
 ''' Check if the histogram of the population (sum of all elements in each sample) is uniform '''
@@ -120,19 +120,19 @@ axes[0, 2].set_title("Short samples - 100 elements per sample")
 print("\n100 samples of 100 elements (short sample size)")
 print("global mean x_3: ", x_3_mean)
 print("means mean x_3: ", x_3_means_mean)
-print("SE of pop:", np.std(x_3_pop))
+print("SD of pop:", np.std(x_3_pop))
 
 print()
 sample_x_3 = random.choice(x_3)
 sem_3 = np.std(sample_x_3)/(100**0.5)
 sample_3_mean = sum(sample_x_3)/len(sample_x_3)
-print("SEM: ", sem_3, "Mean of means SE:", np.std(x_3_means))
+print("SEM: ", sem_3, "Mean of means SD:", np.std(x_3_means))
 #95% probability
 print("Result by SE: Mean = ", x_3_means_mean, " +/- ", 1.96*np.std(x_3_means))
-print("Result by SEM: Mean = ", sample_3_mean, " +/- ", 1.96*sem_3)
+print("Result by SEM: Mean = ", sample_3_mean, "(sample mean) +/- ", 1.96*sem_3)
 print()
-print("Results diff: \nSEM Expected <= ", sem_3*1.96, "\nSEM = ", abs(x_3_mean-sample_3_mean), "\nSE Expected <= ", 1.96*np.std(x_3_means), "\nSE = ", abs(x_3_mean-x_3_means_mean))
+print("Results diff: \nExpected result by SEM <= ", sem_3*1.96, "\nResult by SEM = ", abs(x_3_mean-sample_3_mean), "\nExpected result by SE <= ", 1.96*np.std(x_3_means), "\nResult by SE = ", abs(x_3_mean-x_3_means_mean))
 
-#Obs: SEM is calculated over the elements of a single sample and this number approaches the SE of the mean of means if the sample is large enough.
+#Obs: SEM is calculated over the elements of a single sample and this number approaches the SD of the mean of means if the sample is large enough because the SD == SEM when the population is normally distributed (in this case our population is the means by which by TLC is expected to be normally distributed)
 
 plt.show()
